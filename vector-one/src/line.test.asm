da5ff5ae5220c59bd96dda4e4ed02438a38f45a6
catmeows <you@example.com>
2015-02-06 22:35:04 +0100

    005 Test enhanced to all 4 y-based octants

--- vector-one/src/line.test.asm
+++ vector-one/src/line.test.asm
@@ -2,11 +2,38 @@
 
 	.MODULE test_line
 
-	call vec1_cls
-	ld b, 128
-	ld c, 16
-	ld d, 150
-	ld e, 99
+	call vec1_cls	
+
+	ld b, 123
+	ld c, 4
+	ld d, 38
+	ld e, 112
+_loop	
+	push bc
+	push de
 	call vec1_line
+	pop de
+	pop bc
+	ld a, 5
+	add a, d
+	ld d, a
+	jr nc, _loop
+
+	ld b, 64
+	ld c, 11
+	ld d, 144
+	ld e, 126
+
+_loop2
+	push bc
+	push de
+	call vec1_line
+	pop de
+	pop bc
+	ld a, 5
+	add a, b
+	ld b, a
+	jr nc, _loop2
+
 	call vec1_copy
 	ret
